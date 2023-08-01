32cd5c7e23b54d4d44d4434fe527fe61d165fa3e
RinesaGrabovci8 <rinesagrabovci08@gmail.com>
2023-08-02 00:23:48 +0300

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header';
 import Sidebar from './Components/Sidebar';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header>
+      </Header>
+      <Sidebar></Sidebar>
+      <Footer></Footer>
     </div>
   );
 }
